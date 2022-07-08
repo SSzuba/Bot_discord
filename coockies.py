c9d05ae1837dac927a15b75a9f3b530787cd97f7
SzubaSebastian <sebastex123@gmail.com>
2022-07-08 20:11:01 +0200

    sport newses updates

--- coockies.py
+++ coockies.py
@@ -1,49 +1,17 @@
+import re
 
-accept_list = [
-    "Zgadzam się",
-    "Akceptuję",
-    "Akceptuj wszystko",
-    "Akceptuję i przechodzę do serwisu",
-    "Przejdź do serwisu",
-    "Zaakceptuj wszystko"
-]
+reTypes = ["^Akcept", "^Prze", "Zgadzam", "Wyr", "^Zaakceptuj"]
 
 def coockies_accept(buttons):
 
     for b in buttons:
-        if str(b.text) == "Zgadzam się".upper(): 
-            b.click()
-            break
-        elif str(b.text) == "Akceptuję".upper():
-            b.click()
-            break
-        elif str(b.text) == "Akceptuj wszystko".upper(): 
-            b.click()
-            break
-        elif str(b.text) == "Akceptuję i przechodzę do serwisu".upper(): 
-            b.click()
-            break
-        elif str(b.text) == "Przejdź do serwisu".upper(): 
-            b.click()
-            break
-        elif str(b.text) == "Zaakceptuj wszystko".upper(): 
-            b.click()
-            break
-        elif str(b.text) == "Zgadzam się": 
-            b.click()
-            break
-        elif str(b.text) == "Akceptuję": 
-            b.click()
-            break
-        elif str(b.text) == "Akceptuj wszystko": 
-            b.click()
-            break
-        elif str(b.text) == "Akceptuję i przechodzę do serwisu": 
-            b.click()
-            break
-        elif str(b.text) == "Przejdź do serwisu": 
-            b.click()
-            break
-        elif str(b.text) == "Zaakceptuj wszystko": 
-            b.click()
-            break
+        for t in reTypes:
+            if re.search(str(b.text),t): 
+                b.click()
+                break
+            elif re.search(str(b.text).upper,t): 
+                b.click()
+                break
+            elif re.search(str(b.text).lower,t): 
+                b.click()
+                break
--- sport_news.py
+++ sport_news.py
@@ -5,6 +5,8 @@ import os
 from sites import sites, sport_sites
 from coockies import coockies_accept
 from difflib import SequenceMatcher
+import re
+from datetime import datetime
 
 os.remove("db.json")
 database = db.getDb("db.json")
@@ -12,39 +14,57 @@ database = db.getDb("db.json")
 driver = webdriver.Chrome()
 
 def get_sport_articles():
-
     for s in sites_list:
         driver.get(s)
         driver.find_element_by_tag_name("body").send_keys(Keys.END)
         buttons = driver.find_elements_by_tag_name("button")
         coockies_accept(buttons)
         counter = 0
-        tags = ['h1', 'h2', 'h3', 'h4']
-        for t in tags:
+        articlesTags = ['h1', 'h2', 'h3', 'h4', 'a']
+        timeTags = ['a', 'span', 'time']
+        for t in articlesTags:
             driver.find_element_by_tag_name("body").send_keys(Keys.END)
             articles = driver.find_elements_by_tag_name(t)
             for a in articles:
                 title = str(a.text)
-                if title != '' and len(title) > 20 and counter < 5:
+                if title != '' and len(title) > 40 and counter < 1:
                     links = driver.find_elements_by_tag_name("a")
                     for l in links:
                         linkUrl = str(l.get_attribute('href'))
-                        if linkUrl and SequenceMatcher(None, title, linkUrl).ratio() > 0.3:
-                            q = {"title": title}
-                            data = database.getByQuery(query=q)
-                            if data:
-                                continue
-                            else:
-                                item = {
-                                    "title": title,
-                                    "url": linkUrl,
-                                    "type": 'sport',
-                                    "site": s
-                                }
-                                helpData = item
-                                if SequenceMatcher(None, data, item).ratio() < 0.1:
-                                    database.add(item)
-                                    counter += 1
+                        if linkUrl and SequenceMatcher(None, title, linkUrl).ratio() > 0.3 and counter < 1:
+                            driver.get(linkUrl)
+                            link = driver.current_url
+                            for ti in timeTags:
+                                elements = driver.find_elements_by_tag_name(ti)
+                                for e in elements:
+                                    reg = '([0-1]?[0-9]|2[0-3]):[0-5][0-9]$'
+                                    time = re.search(reg, str(e.text))
+                                    date = re.search("dzisiaj", str(e.text))   
+                                    if date and counter < 1:                                                
+                                        timeAdd = date.string.replace("dzisiaj", datetime.today().strftime('%Y-%m-%d'))
+                                        item = {
+                                            "title": title,
+                                            "url": link,
+                                            "timeAdd": timeAdd,
+                                            "type": 'sport',
+                                            "site": s
+                                        }
+                                        database.add(item)
+                                        counter += 1
+                                        driver.get(s)
+                                        break
+                                    elif time and counter < 1:
+                                        item = {
+                                            "title": title,                                                    
+                                            "url": link,
+                                            "timeAdd": time.string,
+                                            "type": 'sport',
+                                            "site": s
+                                        }
+                                        database.add(item)
+                                        counter += 1
+                                        driver.get(s)
+                                        break
 
 
 sites_list = []
@@ -54,9 +74,11 @@ for s in sites:
 
 for s in sport_sites:
     sites_list.append("https://" + s)
-
+sites_list.reverse()
 get_sport_articles()
 
 
-# driver.close()
-# driver.quit()
+driver.close()
+driver.quit()
+
+#akceptacje coockies tvp(pobieranie), wprost, ?interia?, wp(tytul)
