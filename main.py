b8705fad256ea1d3cb7413b140c516141fccdce6
Szubas <szubas99@gmail.com>
2023-01-02 17:20:21 +0100

    infinity loop with messeges on discord

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import discord
 import use_data
-import get_data
-from discord.ext import commands
+from difflib import SequenceMatcher
+from pysondb import db
+from selenium import webdriver
+from discord.ext import commands, tasks
+from sites import sport_sites_list, business_sites_list, moto_sites_list
 
 TOKEN = 'OTYxOTIyMzI4MDAwODg0NzM3.GwNZZq.lynrZbGfi1uk-lVEFyKIdTZAQO5BYAXDg3rT00'
 
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="!")
-
+database = db.getDb("db.json")
+databaseDet = db.getDb("details.json")
+driver = webdriver.Chrome()
 
 @bot.event
 async def on_ready():
@@ -30,7 +35,119 @@ async def biznes(ctx):
 async def motoryzacja(ctx):
     msg = use_data.get_articles('moto')
     await ctx.send(str(msg))
-    
+
+
+@bot.command()
+async def follow(ctx):
+    while(True):
+        await get_articles(sport_sites_list, 'sport', ctx)
+        get_article_details('sport')
+        await get_articles(moto_sites_list, 'moto', ctx)
+        get_article_details('moto')
+        await get_articles(business_sites_list, 'biznes', ctx) 
+        get_article_details('biznes')
+        await check_for_updates(ctx)
+
+async def get_articles(list_name, type_name, ctx):
+    for s in list_name:
+        driver.get(s)
+        articlesTags = ['h1', 'h2', 'h3', 'h4', 'a']
+        linkChecker = ''
+        titleChecker = ''
+        c = 0
+        for t in articlesTags:
+            articles = driver.find_elements_by_tag_name(t)
+            for a in articles:        
+                try:
+                    title = str(a.text)
+                    if title != '' and len(title) > 50:
+                        links = driver.find_elements_by_tag_name("a")
+                        for l in links:
+                            linkUrl = str(l.get_attribute('href'))
+                            data = database.getAll()
+                            if len(linkUrl) > 50 and SequenceMatcher(None, title, linkUrl).ratio() > 0.3:
+                                item = {
+                                    "title": title,
+                                    "url": linkUrl,
+                                    "type": type_name
+                                }
+                                for d in range(len(data)):
+                                    linkChecker = data[d]["url"]
+                                    titleChecker = data[d]["title"]
+                                    try:
+                                        if linkChecker == linkUrl and SequenceMatcher(None, linkChecker, linkUrl).ratio() > 0.3:
+                                            c = 1
+                                        elif titleChecker == title and SequenceMatcher(None, titleChecker, title).ratio() > 0.3:
+                                            c = 1
+                                    except:
+                                        c = 0
+                                if c == 0:
+                                    database.add(item)
+                                    await ctx.send("NEW! " + title + " " + linkUrl)
+                                else:
+                                    c = 0
+                                    break
+                except:
+                    break
+
+def get_article_details(type_name):
+    infoTags = ['article', 'div', 'p', 'a', 'span']
+    data = database.getByQuery({"type": type_name})
+    dataCheck = databaseDet.getByQuery({"type": type_name})
+    for d in range(len(data)):
+        c = 0
+        counter = 0
+        url = data[d]["url"]
+        title = data[d]["title"]
+        try:
+            if str(dataCheck[d]["url"]) == url:
+                c = 1
+        except:
+            c = 0
+        if c == 0:
+            try:
+                driver.get(url)
+                for t in infoTags:
+                    elements = driver.find_elements_by_tag_name(t)
+                    for e in elements:
+                        if counter < 1 and len(e.text) > 500:
+                            item = {
+                                "title": title,
+                                "url": url,
+                                "info": str(e.text)[0:1000],
+                                "type": type_name
+                            }
+                            databaseD.add(item)
+                            counter += 1
+                        else:
+                            break
+            except:
+                break
+
+
+async def check_for_updates(ctx):
+    infoTags = ['article', 'div', 'p', 'a', 'span']
+    data = databaseDet.getAll()
+    for d in range(len(data)):
+        counter = 0
+        url = data[d]['url']
+        title = data[d]['title']
+        infoChecker = data[d]['info']
+        driver.get(url)
+        for t in infoTags:
+            try:
+                elements = driver.find_elements_by_tag_name(t)
+                for e in elements:
+                    for check in range(len(data)):
+                        if counter < 1 and len(e.text) > 500 and infoChecker != str(e.text)[0:1000]:
+                            dataId = data[d]['id']
+                            databaseDet.updateById(dataId,{"info":str(e.text)[0:1000]})
+                            await ctx.send("UPDATE! " + title + " " + url)
+                            counter += 1
+                        else:
+                            break
+            except:
+                break
 
 # @bot.command()
 # async def szukaj(ctx, *args):
--- use_data.py
+++ use_data.py
@@ -1,42 +1,35 @@
 from pysondb import db
-import re 
-from datetime import datetime 
+import re
+from datetime import datetime
 from difflib import SequenceMatcher
+import pandas as pd
+from fuzzywuzzy import fuzz
 
 database = db.getDb("details.json")
 
 
-def get_newest_article():
-    data = database.getByQuery({"type": "sport"})
-    reg = '([0-1]?[0-9]|2[0-3]):[0-5][0-9]$'
-    temp = 0 
-    for d in range(len(data)):
-        time = data[d]["time"]
-        time = re.search(reg, str(time))
-        time = str(datetime.strptime(time.group(), '%H:%M').time())
-        timeAsNumber = int(time.replace(":",""))
-        if temp < timeAsNumber:
-            temp = timeAsNumber
-            msg = data[d]["title"] + " " + data[d]["url"]
-        
-    return msg
-        
-
 def get_articles(type):
-    data = database.getByQuery({"type": str(type)})
-    title = data[len(data)-1]["title"]
-    url = data[len(data)-1]["url"]
-    return title + " " + url
+        data = database.getByQuery({"type": str(type)})
+        title = data[len(data)-1]["title"]
+        url = data[len(data)-1]["url"]
+        return title + " " + url
+
 
 def get_articles_by_title(title):
     data = database.getAll()
     for i in range(len(data)):
         dataTitle = data[i]['title']
         url = data[i]['url']
-        # if dataTitle.find(str(title)):
-        if SequenceMatcher(None, str(dataTitle), str(title)).ratio() > 0.2:
-            return dataTitle + ' ' + url
-        else:
-            return 'Brak artykułów' 
+        # dodać sprawdzenie
 
-print(get_articles_by_title('Polska'))
+        # if dataTitle.find(str(title)) > 10:
+        if SequenceMatcher(None, str(dataTitle), str(title)).ratio() > 0.3:
+            # print(fuzz.ratio(title, dataTitle))
+            # if fuzz.ratio(title, dataTitle) >= 30:
+            return dataTitle + ' ' + url
+            # print(msg)
+        else:
+            msg = 'Brak artykułów'
+    return msg
+
+# print(get_articles_by_title('leo messi nie trenuje'))
