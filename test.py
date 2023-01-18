055eeb5dfe97773b9a00a50d0dae7eab4f69a483
Szubas <szubas99@gmail.com>
2023-01-18 11:15:28 +0100

    working version

--- test.py
+++
@@ -1,8 +0,0 @@
-import re 
-from datetime import datetime
-
-reg = '([0-1]?[0-9]|2[0-3]):[0-5][0-9]$'
-time = re.search(reg, "dzisiaj 21:32") 
-time = datetime.strptime(time.group(), '%H:%M').time()
-time = str(time)
-print(time.replace(":",""))
--- use_data.py
+++ use_data.py
@@ -1,9 +1,6 @@
 from pysondb import db
-import re
 from datetime import datetime
 from difflib import SequenceMatcher
-import pandas as pd
-from fuzzywuzzy import fuzz
 
 database = db.getDb("details.json")
 
@@ -13,23 +10,3 @@ def get_articles(type):
         title = data[len(data)-1]["title"]
         url = data[len(data)-1]["url"]
         return title + " " + url
-
-
-def get_articles_by_title(title):
-    data = database.getAll()
-    for i in range(len(data)):
-        dataTitle = data[i]['title']
-        url = data[i]['url']
-        # dodać sprawdzenie
-
-        # if dataTitle.find(str(title)) > 10:
-        if SequenceMatcher(None, str(dataTitle), str(title)).ratio() > 0.3:
-            # print(fuzz.ratio(title, dataTitle))
-            # if fuzz.ratio(title, dataTitle) >= 30:
-            return dataTitle + ' ' + url
-            # print(msg)
-        else:
-            msg = 'Brak artykułów'
-    return msg
-
-# print(get_articles_by_title('leo messi nie trenuje'))
